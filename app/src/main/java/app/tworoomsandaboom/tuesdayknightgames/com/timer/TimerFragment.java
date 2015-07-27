4636972ce52258fbbbe14b1c253995a765695a3a
Dillon Bly <dillonbly@gmail.com>
2015-07-26 19:18:41 -0700

    Refactor timer logic and add ability to pause.

--- app/src/main/java/app/tworoomsandaboom/tuesdayknightgames/com/timer/TimerFragment.java
+++ app/src/main/java/app/tworoomsandaboom/tuesdayknightgames/com/timer/TimerFragment.java
@@ -18,7 +18,9 @@ public class TimerFragment extends Fragment {
 
     private TextView mCountDownText;
     private ImageButton mGoBtn;
-    private CountDownTimer mCountDownTimer;
+    private RoundTimer mRoundTimer;
+    private ImageButton mForwardBtn;
+    private ImageButton mBackButton;
 
     public TimerFragment() {
     }
@@ -27,49 +29,89 @@ public class TimerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        mCountDownTimer = new CountDownTimer(5 * 60 * 1000, 100) {
-            @Override public void onTick(long millisUntilFinished) {
-                long secondsRemaining = millisUntilFinished / 1000;
-                mCountDownText.setText(String.format("%d:%02d", secondsRemaining / 60, secondsRemaining % 60));
-            }
-
-            @Override public void onFinish() {
-                Toast toast = Toast.makeText(getActivity(), "Times up!", Toast.LENGTH_LONG);
-                toast.show();
-            }
-        };
+        mRoundTimer = new RoundTimer(3);
 
         View view = inflater.inflate(R.layout.fragment_timer, container, false);
         mCountDownText = (TextView) view.findViewById(R.id.countDownText);
 
         mGoBtn = (ImageButton) view.findViewById(R.id.go_btn);
-        mGoBtn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                startTimer();
-                Toast toast = Toast.makeText(getActivity(), "Start!", Toast.LENGTH_LONG);
-                toast.show();
-            }
-        });
+        mGoBtn.setOnClickListener(new GoButtonAction());
 
-        final ImageButton backBtn = (ImageButton) view.findViewById(R.id.back_btn);
-        backBtn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Perform action on click
-            }
-        });
+        mBackButton = (ImageButton) view.findViewById(R.id.back_btn);
+        mBackButton.setOnClickListener(new BackButtonAction());
 
-        final ImageButton forwardBtn = (ImageButton) view.findViewById(R.id.forward_btn);
-        backBtn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Perform action on click
-            }
-        });
+        mForwardBtn = (ImageButton) view.findViewById(R.id.forward_btn);
+        mForwardBtn.setOnClickListener(new ForwardButtonAction());
 
         return view;
     }
 
-    private void startTimer() {
-        mCountDownTimer.start();
+    private class RoundTimer {
+        private final int minutes;
+        private long millisRemaining;
+        private CountDownTimer activeTimer;
+        private boolean started;
+
+        RoundTimer(int minutes) {
+            this.minutes = minutes;
+            this.millisRemaining = minutes * 60 * 1000;
+        }
+
+        void start() {
+            activeTimer = new CountDownTimer(millisRemaining, 100) {
+                @Override public void onTick(long millisUntilFinished) {
+                    millisRemaining = millisUntilFinished;
+                    updateCountDownText(millisUntilFinished);
+                }
+
+                @Override public void onFinish() {
+                    Toast toast = Toast.makeText(getActivity(), "Time's up!", Toast.LENGTH_LONG);
+                    toast.show();
+                }
+            }.start();
+            this.started = true;
+        }
+
+        void pause() {
+            activeTimer.cancel();
+            this.started = false;
+        }
+
+        void reset() {
+            this.millisRemaining = minutes * 60 * 1000;
+            updateCountDownText(this.millisRemaining);
+            this.started = false;
+        }
+
+        void updateCountDownText(long millisRemaining) {
+            long secondsRemaining = millisRemaining / 1000;
+            mCountDownText.setText(String.format("\n\n%d:%02d", secondsRemaining / 60, secondsRemaining % 60));
+        }
     }
 
+    private class GoButtonAction implements View.OnClickListener {
+        @Override public void onClick(View v) {
+            if (mRoundTimer.started) {
+                mRoundTimer.pause();
+                Toast toast = Toast.makeText(getActivity(), "Paused", Toast.LENGTH_LONG);
+                toast.show();
+            } else {
+                mRoundTimer.start();
+                Toast toast = Toast.makeText(getActivity(), "Start!", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        }
+    }
+
+    private class ForwardButtonAction implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+        }
+    }
+
+    private class BackButtonAction implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+        }
+    }
 }
