1d27bc21586f5019b6d34e5daf0eb0919a6f6f48
Dillon Bly <dillonbly@gmail.com>
2015-07-26 18:31:06 -0700

    Add initial countdown timer to fragment.

--- app/src/main/java/app/tworoomsandaboom/tuesdayknightgames/com/timer/Timer.java
+++ app/src/main/java/app/tworoomsandaboom/tuesdayknightgames/com/timer/Timer.java
@@ -5,15 +5,15 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
 public class Timer extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
-    }
 
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- app/src/main/java/app/tworoomsandaboom/tuesdayknightgames/com/timer/TimerFragment.java
+++ app/src/main/java/app/tworoomsandaboom/tuesdayknightgames/com/timer/TimerFragment.java
@@ -1,16 +1,24 @@
 package app.tworoomsandaboom.tuesdayknightgames.com.timer;
 
+import android.os.CountDownTimer;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
- * A placeholder fragment containing a simple view.
+ * Contains the countdown timer and controls.
  */
 public class TimerFragment extends Fragment {
+
+    private TextView mCountDownText;
+    private ImageButton mGoBtn;
+    private CountDownTimer mCountDownTimer;
 
     public TimerFragment() {
     }
@@ -18,6 +26,50 @@ public class TimerFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_timer, container, false);
+
+        mCountDownTimer = new CountDownTimer(5 * 60 * 1000, 100) {
+            @Override public void onTick(long millisUntilFinished) {
+                long secondsRemaining = millisUntilFinished / 1000;
+                mCountDownText.setText(String.format("%d:%02d", secondsRemaining / 60, secondsRemaining % 60));
+            }
+
+            @Override public void onFinish() {
+                Toast toast = Toast.makeText(getActivity(), "Times up!", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        };
+
+        View view = inflater.inflate(R.layout.fragment_timer, container, false);
+        mCountDownText = (TextView) view.findViewById(R.id.countDownText);
+
+        mGoBtn = (ImageButton) view.findViewById(R.id.go_btn);
+        mGoBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startTimer();
+                Toast toast = Toast.makeText(getActivity(), "Start!", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+
+        final ImageButton backBtn = (ImageButton) view.findViewById(R.id.back_btn);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+            }
+        });
+
+        final ImageButton forwardBtn = (ImageButton) view.findViewById(R.id.forward_btn);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+            }
+        });
+
+        return view;
     }
+
+    private void startTimer() {
+        mCountDownTimer.start();
+    }
+
 }
