364fcb6afaa3ca8f29bfc4f60ec319e8bb705a12
Dillon Bly <dillonbly@gmail.com>
2015-08-23 17:41:29 -0700

    Adding a settings fragment for changing round settings.

---
+++ app/src/main/java/app/tworoomsandaboom/tuesdayknightgames/com/timer/SettingsActivityFragment.java
@@ -0,0 +1,36 @@
+package app.tworoomsandaboom.tuesdayknightgames.com.timer;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+
+/**
+ * Fragment for changing round settings.
+ */
+public class SettingsActivityFragment extends Fragment {
+
+    public SettingsActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        Spinner spinner = (Spinner) view.findViewById(R.id.num_round_spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(view.getContext(),
+                R.array.round_options, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+        return view;
+    }
+}
